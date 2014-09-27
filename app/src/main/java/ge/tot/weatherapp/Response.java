d6dfae537b6dfc9b5898359d625a11ca86872067
Alex Korovyansky <korovyansk@gmail.com>
2014-09-27 15:34:49 +0400

    Fetching real data

---
+++ app/src/main/java/ge/tot/weatherapp/Response.java
@@ -0,0 +1,63 @@
+package ge.tot.weatherapp;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Response {
+ 
+    public static class RForecast {
+ 
+        public static class Temp {
+            @SerializedName("day")
+            public final double day;
+ 
+            @SerializedName("night")
+            public final double night;
+ 
+            public Temp(double day, double night) {
+                this.day = day;
+                this.night = night;
+            }
+        }
+ 
+        public static class Weather {
+            @SerializedName("icon")
+            public final String icon;
+ 
+            @SerializedName("description")
+            public final String description;
+ 
+            public Weather(String icon, String description) {
+                this.icon = icon;
+                this.description = description;
+            }
+        }
+ 
+        @SerializedName("dt")
+        public final long dt;
+ 
+        @SerializedName("temp")
+        public final Temp temp;
+ 
+        @SerializedName("weather")
+        public final List<Weather> weatherList;
+ 
+        public RForecast(long dt, Temp temp, List<Weather> weatherList) {
+            this.dt = dt;
+            this.temp = temp;
+            this.weatherList = weatherList;
+        }
+    }
+ 
+    @SerializedName("cod")
+    public final String code;
+ 
+    @SerializedName("list")
+    public final List<RForecast> RForecastList;
+ 
+    public Response(String code, List<RForecast> RForecastList) {
+        this.code = code;
+        this.RForecastList = RForecastList;
+    }
+}
--- app/src/main/java/ge/tot/weatherapp/WeatherListFragment.java
+++ app/src/main/java/ge/tot/weatherapp/WeatherListFragment.java
@@ -1,14 +1,24 @@
 package ge.tot.weatherapp;
 
 
-
 import android.app.Fragment;
 import android.app.ListFragment;
-import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -18,21 +28,39 @@ import java.util.List;
  */
 public class WeatherListFragment extends ListFragment {
 
+    public class LoadWeatherTask extends AsyncTask<Void, Void, List<Forecast>> {
+
+        @Override
+        protected List<Forecast> doInBackground(Void... params) {
+            String responseJson = loadJson();
+            return transformJson(responseJson);
+        }
+
+        @Override
+        protected void onPostExecute(List<Forecast> result) {
+            super.onPostExecute(result);
+            if (result == null) {
+                setListAdapter(null);
+                setEmptyText("Unexpected error. Please check your network connection");
+            } else {
+                ArrayAdapter<Forecast> adapter = new ArrayAdapter<Forecast>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        result);
+                setListAdapter(adapter);
+                setListShown(true);
+            }
+        }
+    }
 
     public WeatherListFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        List<Forecast> forecasts = Forecast.makeRandom(7);
-
-        ArrayAdapter<Forecast> adapter = new ArrayAdapter<Forecast>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                forecasts);
-        setListAdapter(adapter);
+    public void onStart() {
+        super.onStart();
+        new LoadWeatherTask().execute();
     }
 
     @Override
@@ -40,5 +68,84 @@ public class WeatherListFragment extends ListFragment {
         super.onListItemClick(l, v, position, id);
         Forecast forecast = (Forecast) getListAdapter().getItem(position);
         BusProvider.getBus().post(new WeatherItemClickedEvent(forecast));
+    }
+
+    private String loadJson() {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are available at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Tbilisi&mode=json&units=metric&cnt=7&lang=en");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                forecastJsonStr = null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                forecastJsonStr = null;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attempting
+            // to parse it.
+            forecastJsonStr = null;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+        return forecastJsonStr;
+    }
+
+    private List<Forecast> transformJson(String responseJson) {
+        if (responseJson == null) {
+            return null;
+        }
+        Response response = new Gson().fromJson(responseJson, Response.class);
+        List<Forecast> forecasts = new ArrayList<Forecast>();
+        for (Response.RForecast rForecast : response.RForecastList) {
+            Date date = new Date(rForecast.dt);
+            double dayTemp = rForecast.temp.day;
+            double nightTemp = rForecast.temp.night;
+            String iconUrl = rForecast.weatherList.get(0).description;
+            String description = rForecast.weatherList.get(0).description;
+            forecasts.add(new Forecast(date, description, nightTemp, dayTemp, iconUrl));
+        }
+        return forecasts;
     }
 }
