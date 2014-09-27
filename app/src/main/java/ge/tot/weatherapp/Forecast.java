03e38fe5398c25eb6bc1f546a2b667961303786f
Alex Korovyansky <korovyansk@gmail.com>
2014-09-27 11:32:17 +0400

    Add Forecast

--- app/src/main/java/ge/tot/weatherapp/Forecast.java
+++ app/src/main/java/ge/tot/weatherapp/Forecast.java
@@ -3,9 +3,51 @@
  */
 package ge.tot.weatherapp;
 
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Forecast
  */
 public class Forecast {
+    private final Date date;
+    private final String description;
+    private final double nightTemp;
+    private final double dayTemp;
+    private final String iconUrl;
 
+    public static Forecast makeRandom(Date date) {
+        Random random = new Random(System.currentTimeMillis());
+        double dayTemp = random.nextDouble() * 10.0;
+        double nightTemp = random.nextDouble() * 5.0;
+        return new Forecast(date, "Random", dayTemp, nightTemp, null);
+    }
+
+    public Forecast(Date date, String description, double nightTemp, double dayTemp, String iconUrl) {
+        this.date = date;
+        this.description = description;
+        this.nightTemp = nightTemp;
+        this.dayTemp = dayTemp;
+        this.iconUrl = iconUrl;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getNightTemp() {
+        return nightTemp;
+    }
+
+    public double getDayTemp() {
+        return dayTemp;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
 }
