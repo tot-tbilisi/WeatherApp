5ce6bded6604be3ba7b5e001156d2cde07de486e
Alex Korovyansky <korovyansk@gmail.com>
2014-09-30 13:45:27 +0400

    add super espresso test

--- app/src/androidTest/java/ge/tot/weatherapp/BasicUITest.java
+++ app/src/androidTest/java/ge/tot/weatherapp/BasicUITest.java
@@ -2,9 +2,16 @@ package ge.tot.weatherapp;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-import junit.framework.Assert;
-
 import ge.tot.weatherapp.ui.MyActivity;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.startsWith;
 
 /**
  * BasicUITest
@@ -15,8 +22,15 @@ public class BasicUITest extends ActivityInstrumentationTestCase2<MyActivity> {
         super(MyActivity.class);
     }
 
-    public void testListItemClick() {
-        Assert.assertEquals(true, false);
+
+    public void testListItemClick() throws Exception {
+        getActivity();
+        onData(hasToString(startsWith("Tue"))).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.forecast_description))
+                .check(matches(isDisplayed()));
     }
+
+
 
 }
